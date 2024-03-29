1c675fb1b75adabe349293c462e41b48dde71990
rajeevrazz28 <ranjanrajeevasgkrs@gmail.com>
2024-03-30 01:32:39 +0530

    first commit

---
+++ backend/routes/User.js
@@ -0,0 +1,42 @@
+
+const express = require('express');
+const router = express.Router();
+const User = require('../Models/User');
+
+
+router.post('/', async (req, res) => {
+  const { username } = req.body;
+
+  try {
+    let user = new User({ username });
+    await user.save();
+    res.json(user);
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send('Server error');
+  }
+});
+
+
+router.get('/', async (req, res) => {
+  try {
+    const users = await User.find().select('-password');
+    res.json(users);
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send('Server error');
+  }
+});
+
+
+router.delete('/:id', async (req, res) => {
+  try {
+    await User.findByIdAndDelete(req.params.id);
+    res.json({ message: 'User deleted' });
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send('Server error');
+  }
+});
+
+module.exports = router;
